81a3a19b56bb630e7b70708b8cf05cd63d6e3bf2
cucu <1191810490@qq.com>
2015-09-12 21:35:20 +0800

    Signed-off-by: cucu <1191810490@qq.com>

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//mysql database address
+define('DB_HOST','sql202.byethost7.com');
+//mysql database user
+define('DB_USER','b7_16505701');
+//database password
+define('DB_PASSWD','a12345');
+//database name
+define('DB_NAME','b7_16505701_2');
+//database prefix
+define('DB_PREFIX','emlog_');
+//auth key
+define('AUTH_KEY','0Z@avJfJk^wIBGlDHLA8m!AZv1R%lz*D8055889462e3b4f6b1424b0f9ef72005');
+//cookie name
+define('AUTH_COOKIE_NAME','EM_AUTHCOOKIE_YvQSYjSs4QkXwEyQTUdXcxwDMyPENVYd');
